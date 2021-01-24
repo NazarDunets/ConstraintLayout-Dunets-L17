41850ed288fafa95dd5987938bd27b237b085e53
NazarDunets <jcq.nazardunets@gmail.com>
2021-01-24 13:26:57 +0200

    Done!

--- app/src/main/java/com/example/constraintlayout_dunets_l17/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout_dunets_l17/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.constraintlayout_dunets_l17
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
